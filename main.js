5d851a1160c673c804997b3c8ae3e822f518c243
prakrutivaghasiya <prakrutivaghasiya@gecg28.ac.in>
2020-05-17 21:59:37 +0530

    added Search option

--- main.js
+++ main.js
@@ -110,6 +110,22 @@ class Store{
 }
 
 
+// Search function
+function searchBook(val){
+    const books = document.getElementById('book-list');
+    const values = books.querySelectorAll('tr');
+    const regex = /[\s]+/g ;
+    values.forEach((value) => {
+        const text = value.innerText.replace(regex, "").toLowerCase();
+        if(text.indexOf(val) != -1){
+            value.style.display = 'table-row';
+        }else{
+            value.style.display = 'none';
+        }
+    }) 
+}
+
+
 // Adding Events
 
 // Display Books
@@ -145,3 +161,19 @@ document.getElementById('book-list').addEventListener('click',
         Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
         UI.showAlert('BooK Removed', 'success');
     }) 
+
+// Searching Book
+document.getElementById('search').addEventListener('keyup', 
+    (e) => {
+        searchBook(e.target.value.toLowerCase());
+    })
+
+document.getElementById('search-btn').addEventListener('click',
+    (e) => {
+        e.preventDefault();
+        if (e.target.previousElementSibling.value !== ''){
+            searchBook(e.target.previousElementSibling.value.toLowerCase());
+        }else{
+            UI.showAlert('Empty search', 'info');
+        }     
+    })
